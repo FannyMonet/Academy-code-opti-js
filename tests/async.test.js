de38ffdcbe32de6748c5170dc7c1897699df6b5a
Fanny Monet <monet.f@sfeir.com>
2022-07-07 14:48:26 +0200

    feat: async exercice

--- tests/async.test.js
+++ tests/async.test.js
@@ -1,6 +1,11 @@
+import { sumAsyncArrays } from "../src/async";
+
 describe("async exercices", () => {
-  test("should sum two Promises arrays", () => {
+  test("should sum two Promises arrays", async () => {
     // Given
-    const arr1 = Array.from({ length: 10 }, async () => Math.random() * 10);
+    const arr1 = Promise.resolve([1, 2, 3, 4, 5]);
+    const arr2 = Promise.resolve([2, 3, 4, 5, 6]);
+    const sum = await sumAsyncArrays(arr1, arr2);
+    expect(sum).toEqual([3, 5, 7, 9, 11]);
   });
 });
